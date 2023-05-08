b55433373bb3e2d745b808dfa0561ce15d61d396
senik <sebastienwenzel@gmail.com>
2023-05-08 14:46:31 +1100

    set up

---
+++ src/data/voiture.js
@@ -0,0 +1,51 @@
+const datas = [
+
+        {
+    "Voitures": [
+    {
+    "ID": 1,
+    "Modèle": "Renault Clio",
+    "Prix": 50,
+    "Transmission": "Manuelle",
+    "Rating": 4
+    },
+    {
+    "ID": 2,
+    "Modèle": "Volkswagen Golf",
+    "Prix": 65,
+    "Transmission": "Automatique",
+    "Rating": 3
+    },
+    {
+    "ID": 3,
+    "Modèle": "Ford Focus",
+    "Prix": 55,
+    "Transmission": "Manuelle",
+    "Rating": 4
+    },
+    {
+    "ID": 4,
+    "Modèle": "Toyota Corolla",
+    "Prix": 60,
+    "Transmission": "Automatique",
+    "Rating": 5
+    },
+    {
+    "ID": 5,
+    "Modèle": "BMW 3 Series",
+    "Prix": 100,
+    "Transmission": "Automatique",
+    "Rating": 4
+    },
+    {
+    "ID": 6,
+    "Modèle": "Mercedes-Benz E-Class",
+    "Prix": 150,
+    "Transmission": "Automatique",
+    "Rating": 5
+    }
+    ]
+    }
+]
+
+export default datas;
